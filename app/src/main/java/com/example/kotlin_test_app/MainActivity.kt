baae42708b6131d261a832fa1262bfada26ff802
conrad96 <conrad.mugisha@gmail.com>
2019-08-19 15:10:10 +0300

    added minus and add button onto layout

--- app/src/main/java/com/example/kotlin_test_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_test_app/MainActivity.kt
@@ -11,20 +11,6 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //get reference to views
-        var username = findViewById(R.id.et_user_name) as EditText
-        var password = findViewById(R.id.et_password) as EditText
-        var button_login = findViewById(R.id.btn_submit) as Button
-        var button_reset = findViewById(R.id.btn_reset) as Button
 
-        button_reset.setOnClickListener {
-            username.setText("")
-            password.setText("")
-        }
-        button_login.setOnClickListener {
-            val _username = username.text
-            val _password = password.text
-            Toast.makeText(this@MainActivity, _password, Toast.LENGTH_LONG).show()
-        }
     }
 }

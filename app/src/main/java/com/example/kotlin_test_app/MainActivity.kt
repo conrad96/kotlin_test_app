c1df306ba10f32968e80746c746fc77cea6c7aa7
conrad96 <conrad.mugisha@gmail.com>
2019-08-19 15:16:18 +0300

    backend logic to add/subtract value on button press

--- app/src/main/java/com/example/kotlin_test_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_test_app/MainActivity.kt
@@ -10,7 +10,21 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //get values from views
+        var counter = 0;
+        var counter_text = findViewById(R.id.counter_text) as TextView
+        var subtract = findViewById(R.id.minusBtn) as Button
+        var add = findViewById(R.id.plusBtn) as Button
+        //button clicks
+        subtract.setOnClickListener {
+            counter--
+            counter_text.setText(counter)
+        }
 
+        add.setOnClickListener {
+            counter++
+            counter_text.setText(counter)
+        }
 
     }
 }
